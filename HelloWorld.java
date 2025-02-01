a9da61c68b1d962d8284017fd783379bd0cbea46
12325670 <msgsubhash1@gmail.com>
2025-02-01 23:17:53 +0530

    updated java notes

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,9 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+    //    System.out.println("Hello, World!");
+
+        byte a = 5;
+        int b = a ;
+        System.out.println(b);
     }
 }

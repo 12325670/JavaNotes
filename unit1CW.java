b69602dc4754cf9a4044a63603160a34971108c7
12325670 <msgsubhash1@gmail.com>
2025-02-07 15:33:49 +0530

    functions and functions overloading

--- unit1CW.java
+++ unit1CW.java
@@ -14,22 +14,88 @@ public class unit1CW {
 // }
 // System.out.println("You entered: " + var2);
 
-Scanner s = new Scanner(System.in);
-System.out.println("Enter number of students: ");
-int a = s.nextInt();
+// --------------------------
 
-int[] marks = new int[a];
-System.out.println("Enter marks of students: ");
-for(int i=0;i<marks.length;i++){
-    marks[i] =s.nextInt();
-}
-s.close();
-System.out.println("Your Entered Marks are : ");
-System.out.println(Arrays.toString(marks));
-Arrays.sort(marks)
-int len
+// Scanner s = new Scanner(System.in);
+// System.out.println("Enter number of students: ");
+// int a = s.nextInt();
 
+// int[] marks = new int[a];
+// System.out.println("Enter marks of students: ");
+// for(int i=0;i<marks.length;i++){
+//     marks[i] =s.nextInt();
+// }
+// s.close();
+// System.out.println("Your Entered Marks are : ");
+// System.out.println(Arrays.toString(marks));
+// Arrays.sort(marks);
+
+// ========================================================= 2DARRAY====================================
+
+// deepTosting is used to print the 2D array
+
+
+
+// int[][] a = {{10,20,30},{40,50,60}};
+
+// Q: print using for loop-----------------------------------
+// for(int i=0;i<a.length;i++){
+//     for(int j=0;j<a[i].length;j++){
+//         System.out.print(a[i][j]+" ");
+//     }
+//     System.out.println();
+// }
+
+// Q: using for each loop :----------------------------------------
+
+// for(int[] row : a){
+//     for(int num : row){
+//         System.out.print(num+" ");
+//     }
+//     System.out.println();
+//         }
+// =======================Jagged array====================================================
+
+// m1------    using curley braces
+// int[][] a =
+
+
+// ---m2----
+// int[][] a = new int [3][];
+// a[0] = new int[]{10,5};
+// a[1] = new int[]{5,15,21};
+// a[2] = new int[]{1,2,3,4,5};
+
+// for(int i=0;i<a.length;i++){
+//     for(int j=0;j<a[i].length;j++){
+//         System.out.print(a[i][j]+" ");
+//     }
+//     System.out.println();
+// }
+
+// m3------
+
+// System.out.println("Enter number of Rows: ");
+// Scanner s= new Scanner(System.in);
+// int rows = s.nextInt();
+// System.out.println("Enter number of columns: ");
+// int cols = s.nextInt();
+// int[][] jagged = new int[rows][cols];
+// System.out.println("Enter elements of array ");
+// for(int i=0;i<rows;i++){
+//     for(int j=0;j<cols;j++){
+//         jagged[i][j] = s.nextInt();
+//     }
+// }
+
+
+
+
+
+
+    
 }
 }
+
 
 
